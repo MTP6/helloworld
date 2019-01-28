0472c44e1a9250861b79eaa6f9a3203e9c8aec3c
MTP6 <michael@familie-paul.de>
2019-01-28 18:24:26 +0100

    Erstes commit

---
+++ app.js
@@ -0,0 +1,17 @@
+// ************************************************************************************************************
+// Example for hello world
+//
+
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+
+    res.send('Hello World! No 1');
+  
+})
+
+app.listen(3000, function(){
+    console.log('----------------------------------------------------');
+    console.log('Example app listening on port 3000!');
+});
